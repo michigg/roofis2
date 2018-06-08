cd3ac8afe9a20e325aac4b3bf7fe7247d91bd4d9
Tobias Haegele <tobias-sebastian.haegele@stud.uni-bamberg.de>
2018-06-08 21:15:10 +0200

    just testing

--- roofis2/roomservice/views.py
+++ roofis2/roomservice/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.jinja', {})
+    return render(request, 'base.jinja', {"title":"roofis is love roofis is live"})
