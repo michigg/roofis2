7fb4d8d6dade49fe3f41d25ca303845f538d08a8
MG <michael-guenther.goetz@stud.uni-bamberg.de>
2018-06-09 05:58:36 +0200

    Implement new hompage layout

--- roofis2/roomservice/views.py
+++ roofis2/roomservice/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from roomservice.models import Room
+from roomservice.models import Room, Favorite
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 # Create your views here.
 def search(request):
     rooms = Room.objects.all()
-    return render(request, 'search.jinja', {"title":"rooF(i)S is love rooF(i)S is live!!", "rooms":rooms})
+    return render(request, 'search.jinja', {"title": "rooF(i)S is love rooF(i)S is live!!", "rooms": rooms})
 
 
 def booking(request):
@@ -15,7 +16,7 @@ def booking(request):
     room = Room.objects.get(id=room_id)
     logger.info(room_id)
     logger.info(room)
-    return render(request, 'booking.jinja', {"title": "rooF(i)S is love rooF(i)S is live!!","room":room})
+    return render(request, 'booking.jinja', {"title": "rooF(i)S is love rooF(i)S is live!!", "room": room})
 
 
 def admin(request):
@@ -23,4 +24,13 @@ def admin(request):
 
 
 def favorites(request):
+    if request.user.is_authenticated:
+        favorites = Favorite.objects.filter(staff__user=request.user)
+        return render(request, 'favorites.jinja',
+                      {"title": "rooF(i)S is love rooF(i)S is live!!", 'favorites': favorites})
+    return render(request, 'favorites.jinja',
+                  {"title": "rooF(i)S is love rooF(i)S is live!!"})
+
+
+def location_based_search(request):
     return render(request, 'favorites.jinja', {"title": "rooF(i)S is love rooF(i)S is live!!"})
