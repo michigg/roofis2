7f286feaa1d8eb38044e5e11506cc89d67b999ac
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2020-02-04 04:21:09 +0100

    chore: implement lector building api, fix date autoselect

--- src/app.py
+++ src/app.py
@@ -3,16 +3,20 @@ from urllib.parse import urlencode, quote_plus
 
 import requests
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 ROOFIS_API = os.environ.get("ROOFIS_API")
 LECTOR_WEB = os.environ.get("LECTOR_WEB_URL")
+LECTOR_BUILDING_API = os.environ.get("LECTOR_BUILDING_API")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    locations = requests.get(f'{ROOFIS_API}locations/').json()
+    # locations = requests.get(f'{LECTOR_BUILDING_API}building/').json()
+
     if request.method == 'POST':
         form, location, min_size, start_date, start_time = get_validated_form_data()
 
@@ -22,14 +26,16 @@ def index():
             response = requests.get(url)
             if response.status_code == 200:
                 empty_results = True if not response.json() else False
-                return render_template("index.jinja2", free_rooms=response.json(), locations=locations, form=form,
-                                       empty_results=empty_results, lector_url=LECTOR_WEB)
+                return render_template("index.jinja2", free_rooms=response.json(), form=form,
+                                       empty_results=empty_results, lector_url=LECTOR_WEB,
+                                       lector_building_api=LECTOR_BUILDING_API)
             else:
                 error_msg = "Zurzeit ist der RooFiS Endpunkt nicht erreichbar. Versuchen Sie es später nochmal, oder kontaktieren Sie den Administrator. "
         else:
             error_msg = "Sie müssen mindestens ein Startdatum und eine Startzeit angeben",
-        return render_template("index.jinja2", error=error_msg, locations=locations, form=form)
-    return render_template("index.jinja2", locations=locations, form={})
+        return render_template("index.jinja2", error=error_msg, form=form,
+                               lector_building_api=LECTOR_BUILDING_API)
+    return render_template("index.jinja2", form={}, lector_building_api=LECTOR_BUILDING_API)
 
 
 def get_validated_form_data():
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,6 +1,18 @@
+const SELECT_BOX_ID = "location-select";
+const SELECT_BOX = document.getElementById(SELECT_BOX_ID);
+
 function initForm() {
     let today = new Date();
-    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let month = (today.getMonth() + 1).toString();
+    if (month.length == 1) {
+        month = "0" + month
+    }
+    let day = today.getDate().toString();
+    if (day.length == 1) {
+        day = "0" + day
+    }
+
+    let date = today.getFullYear() + '-' + month + '-' + day;
     let minutes = today.getMinutes();
     let hours = today.getHours();
     if (minutes >= 30) {
@@ -15,8 +27,10 @@ function initForm() {
     let startDateInput = document.getElementById('startdate');
     let startTimeInput = document.getElementById('starttime');
 
+    console.log("INIT");
     console.log(startDateInput.value);
     console.log(startTimeInput.value);
+    console.log(date);
 
     if (!startDateInput.value) {
         startDateInput.value = date;
@@ -25,6 +39,18 @@ function initForm() {
     if (!startTimeInput.value) {
         startTimeInput.value = time;
     }
+
+    const locationButton = document.querySelector('#position-btn');
+    let url = locationButton.dataset.lectorBuildingApi;
+    fetch(url + "building/")
+        .then((resp) => resp.json())
+        .then(function (data) {
+            // Handle data
+            resetSelectBox(data)
+        }).catch(error => {
+        // Handle error
+        // alert("Orte konnten nicht geladen werden.")
+    });
 
 }
 
@@ -45,3 +71,62 @@ document.addEventListener("DOMContentLoaded", function () {
     initForm();
 });
 
+function loadPositionRankedBuildings() {
+    console.log("START");
+    const locationButton = document.querySelector('#position-btn');
+    let url = locationButton.dataset.lectorBuildingApi;
+
+    if ("geolocation" in navigator) {
+        /* geolocation funktioniert */
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let positionFilteredUrl = url + "building/?coord=" + position.coords.latitude + "," + position.coords.longitude;
+            fetch(positionFilteredUrl)
+                .then((resp) => resp.json())
+                .then(data => {
+                    // Handle data
+                    console.log(data);
+                    changeLocationDropdownContent(data)
+                }).catch(error => {
+                // Handle error
+                // alert("Orte konnten nicht geladen werden.")
+            });
+        });
+    } else {
+        /* geolocation funktioniert NICHT */
+        alert("Geolocation Modul konnte nicht gefunden werden.")
+    }
+}
+
+function clearSelectBox(selectBox) {
+    for (const elem in selectBox.options) {
+        selectBox.remove(0);
+    }
+
+}
+
+function fillSelectBox(data, selectBox) {
+    var option = document.createElement("option");
+    option.value = "";
+    option.text = "Ort auswählen";
+    selectBox.add(option);
+
+    for (const elem of data) {
+        var option = document.createElement("option");
+        option.value = elem;
+        option.text = elem;
+        selectBox.add(option);
+    }
+}
+
+function resetSelectBox(data) {
+    clearSelectBox(SELECT_BOX);
+    fillSelectBox(data, SELECT_BOX);
+}
+
+function changeLocationDropdownContent(data) {
+    resetSelectBox(data);
+
+    if (SELECT_BOX.options.length > 2) {
+        SELECT_BOX.options[1].selected = true;
+    }
+}
