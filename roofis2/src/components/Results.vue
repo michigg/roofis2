c10eaa012e29bce20d22b390f2505eefd2ca0a24
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2020-02-21 21:32:42 +0100

    feat: vue rewrite

--- roofis2/src/components/Results.vue
+++ roofis2/src/components/Results.vue
@@ -1,52 +1,79 @@
 <template>
-    <div v-if="results" class="row justify-content-center mb-5">
-        <div class="col-12 col-xs-11 col-sm-10 col-md-9 col-lg-7 col-xl-5 mt-3 shadow bg-secondary py-3 px-4">
-            <h1 class="text-center">Ergebnisse</h1>
-            <table id="results" class="table table-striped table-bordered" cellspacing="0" style="width:100%">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Raum <i class="fas fa-cube"></i></th>
-                    <th>Typ <i class="fas fa-shapes"></i></th>
-                    <th>Plätze <i class="fas fa-chair"></i></th>
-                    <th>Frei <i class="fas fa-stopwatch"></i></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="room in this.freeRooms" :key="room">
-                    <td>{{ loop.index }}</td>
-                    <td>
-                        <a :href="lector_url/room.building_key/room.floor/room.number">{{ room }}</a>
-                    </td>
-                    <td>{{ room.name }}</td>
-                    <td>{{ room.size }}</td>
-                    <td class="text-center">
-                        {% if room.next_allocation %}
-                        bis {{ room.next_allocation }}
-                        {% else %}
-                        <i class="fas fa-check text-success"></i>
-                        {% endif %}
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+    <div v-if="rooms && rooms.length > 0" class="row justify-content-center mb-5">
+        <b-col cols="12" xs="11" sm="10" md="9" lg="7" xl="5" class="mt-3 shadow bg-secondary py-3 px-4">
+            <h1 class="text-center">{{$t('results.results')}}</h1>
+            <b-table
+                    stacked="sm"
+                    small
+                    sort-icon-right
+                    :fields="fields"
+                    :items="rooms"
+                    :sort-by.sync="sortBy"
+                    :sort-desc.sync="sortDesc"
+            >
+                <template v-slot:head(short)>
+                    <div class="text-left">{{$t('results.room')}}
+                        <b-icon icon="square" font-scale="1.25"></b-icon>
+                    </div>
+                </template>
+                <template v-slot:head(name)>
+                    <div class="text-left">{{$t('results.type')}}
+                        <b-icon-columns-gutters font-scale="1.25"></b-icon-columns-gutters>
+                    </div>
+                </template>
+                <template v-slot:head(size)>
+                    <div class="text-left">{{$t('results.size')}}
+                        <b-iconstack font-scale="1.25">
+                            <b-icon stacked icon="box-arrow-up-right" scale="0.6" shift-v="0.5"
+                                    shift-h="0.5"></b-icon>
+                            <b-icon stacked icon="square"></b-icon>
+                        </b-iconstack>
+                    </div>
+                </template>
+                <template v-slot:head(next_allocation)>
+                    <div class="text-left">{{$t('results.free')}}
+                        <b-icon-clock font-scale="1.25"></b-icon-clock>
+                    </div>
+                </template>
+                <template v-slot:cell(size)="data">
+                    <span v-if="data.value===-1">{{$t('results.notAvailable')}}</span>
+                    <span v-else>{{data.value}}</span>
+                </template>
+                <template v-slot:cell(short)="data">
+                    <div class="text-left">{{data.value}}</div>
+                </template>
+                <template v-slot:cell(name)="data">
+                    <div class="text-left">{{data.value}}</div>
+                </template>
+                <template v-slot:cell(next_allocation)="data">
+                    <span v-if="data.value">{{$t('results.freeUntil')}} {{data.value}}</span>
+                    <span v-else><b-icon icon="check" font-scale="1.5" variant="success"></b-icon></span>
+                </template>
+            </b-table>
+        </b-col>
     </div>
-
-    <div v-else class="row justify-content-center">
-        <div class="col-12 col-xs-11 col-sm-10 col-md-9 col-lg-7 col-xl-5 mt-3 shadow bg-secondary py-3 px-4 text-center">
-            <h1>Ergebnisse</h1>
-            <p>Keine Freien Räume gefunden</p>
-        </div>
+    <div v-else-if="rooms && rooms.length == 0" class="row justify-content-center">
+        <b-col cols="12" xs="11" sm="10" md="9" lg="7" xl="5" class="mt-3 shadow bg-secondary py-3 px-4 text-center">
+            <h1>{{$t('results.results')}}</h1>
+            <p>{{$t('results.noFreePlaces')}}</p>
+        </b-col>
+    </div>
+    <div v-else>
     </div>
 </template>
 
 <script>
     export default {
         name: "Results",
-        computed: {
-            freeRooms: function () {
-                return this.rooms.filter((room) => room.building_key && room.floor && room.number)
+        props: ["rooms"],
+        data() {
+            return {
+                fields: [{key: 'short', sortable: true},
+                    {key: 'name', sortable: true},
+                    {key: 'size', sortable: true},
+                    {key: 'next_allocation', sortable: true}],
+                sortBy: 'short',
+                sortDesc: false,
             }
         }
     }
---
+++ roofis2/src/views/Roofis2.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="container-fluid">
+            <div>
+                <parameter-selection @roomsLoaded="onRoomsLoaded"></parameter-selection>
+                <results :rooms.sync="this.rooms"></results>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ParameterSelection from "../components/ParameterSelection";
+    import Results from "../components/Results";
+
+    export default {
+        name: 'Roofis2',
+        components: {ParameterSelection, Results},
+        data() {
+            return {
+                rooms: null,
+            }
+        },
+        methods: {
+            onRoomsLoaded: function (freeRooms) {
+                this.rooms = freeRooms;
+                this.$emit('update:rooms', freeRooms)
+            }
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
