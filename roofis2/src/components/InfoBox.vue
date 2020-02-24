c282051e80c7238c635a3b249ec193198c107e96
Michael Götz <michael-guenther.goetz@stud.uni-bamberg.de>
2020-02-24 15:23:56 +0100

    chore: add locale support for info box

--- roofis2/src/components/InfoBox.vue
+++ roofis2/src/components/InfoBox.vue
@@ -1,22 +1,27 @@
 <template>
     <b-row v-if="this.openings" class="justify-content-center h-100">
         <b-col cols="12" class="mt-3 shadow bg-secondary py-3 px-4">
-            <h2>Öffnungszeiten:</h2>
-            <b-alert variant="warning" show><small>Diese Daten werden direkt von der <a :href="source">Webseite</a>
-                gelesen und können daher fehlerhaft sein.</small></b-alert>
+            <h2>{{$t('infoBox.headline')}}</h2>
+            <b-alert variant="warning" show>
+                <i18n path="infoBox.contentInformation" tag="small">
+                    <template v-slot:link>
+                        <a :href="source" target="_blank">{{ $t('infoBox.contentInformationLinkText') }}</a>
+                    </template>
+                </i18n>
+            </b-alert>
 
             <b-button v-b-toggle.openings-collapse :aria-expanded="showOpenings ? 'true' : 'false'" variant="primary"
-                      class="rounded-0 w-100">
-                <span class="when-opened">Öffnungszeiten ausblenden</span>
-                <span class="when-closed">Öffnungszeite anzeigen</span>
+                      class="rounded-0 w-100 d-md-none">
+                <span class="when-opened">{{$t('infoBox.hideOpeningsBtnLabel')}}</span>
+                <span class="when-closed">{{$t('infoBox.showOpeningsBtnLabel')}}</span>
             </b-button>
             <b-collapse id="openings-collapse" class="mt-2" v-model="showOpenings">
                 <table class="table text-left table-sm">
                     <thead>
                     <tr>
-                        <th>Ort</th>
-                        <th>Wochentage</th>
-                        <th>Uhrzeit</th>
+                        <th>{{$t('infoBox.tableHeadingLocation')}}</th>
+                        <th>{{$t('infoBox.tableHeadingWeekdays')}}</th>
+                        <th>{{$t('infoBox.tableHeadingTimes')}}</th>
                     </tr>
                     </thead>
                     <tr v-for="opening in openings" :key="opening.location">
@@ -32,14 +37,14 @@
     </b-row>
     <b-row v-else-if="!this.openings && this.$data.UNI_INFO_API" class="justify-content-center h-100">
         <b-col cols="12" class="mt-3 shadow bg-secondary py-3 px-4">
-            <h2>Öffnungszeiten:</h2>
+            <h2>{{$t('infoBox.headline')}}</h2>
             <div v-if="!error">
-                <p>Lade...</p>
+                <p>{{$t('loading')}}</p>
                 <b-spinner variant="primary" type="grow" label="Spinning"
                            style="width: 4rem; height: 4rem;"></b-spinner>
             </div>
             <b-alert v-else variant="danger" show>
-                {{error}}
+                {{$t('infoBox.loadingError')}}
             </b-alert>
         </b-col>
     </b-row>
@@ -53,31 +58,41 @@
             return {
                 openings: null,
                 source: "",
-                error: null,
+                error: false,
                 showOpenings: true,
             }
         },
         mounted() {
             let url = this.$data.UNI_INFO_API.concat('openings/');
-            if (window.innerWidth < 768) {
-                this.showOpenings = false;
-            }
+
+            window.addEventListener('resize', this.onResize);
             this.axios
                 .get(url)
                 .then((response) => {
                     this.openings = response.data.openings;
                     this.source = response.data.source;
-                    this.error = null;
+                    this.error = false;
                 })
                 .catch((error) => {
                     // handle error
                     console.log(error);
-                    this.error = "Es konnten keine Öffnungszeiten geladen werden";
+                    this.error = true
                 })
                 .then(function () {
                     // always executed
                 });
 
+        },
+        methods: {
+            onResize: function () {
+                this.showOpenings = window.innerWidth >= 768;
+            }
+        },
+        created() {
+            this.onResize()
+        },
+        beforeDestroy: function () {
+            window.removeEventListener('resize', this.onResize)
         }
     }
 </script>
